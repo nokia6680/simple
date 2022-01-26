7ef1796e4b6efa48f8ac1b447b0727204004a384
Artur Ballod <artur.ballod@gmail.com>
2022-01-27 01:27:17 +0200

    #art-final-full

--- dev/static/js/slider.js
+++ dev/static/js/slider.js
@@ -21,3 +21,165 @@ const swiper = new Swiper('.net-swiper-container', {
         prevEl: '.net-item__gallery-prev',
     },
 });
+
+const swiper2 = new Swiper('.swiper-gallery-container', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    grabCursor: true,
+    speed: 1000,
+    autoplay: false,
+
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            shadow: false,
+            opacity: 0,
+            translate: ["-5%", 0, -1],
+        },
+        next: {
+            opacity: 0,
+            translate: ["5%", 0, 0],
+        },
+    },
+
+    navigation: {
+        nextEl: '.gallery-next',
+        prevEl: '.gallery-prev',
+    },
+
+    breakpoints: {
+        375: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            grabCursor: true,
+            speed: 1000,
+            autoplay: false,
+
+            effect: "creative",
+            creativeEffect: {
+                prev: {
+                    shadow: false,
+                    opacity: 0,
+                    translate: ["-5%", 0, -1],
+                },
+                next: {
+                    opacity: 0,
+                    translate: ["5%", 0, 0],
+                },
+            },
+
+            pagination: {
+                el: '.swiper-pagination',
+                dynamicBullets: true,
+            },
+        },
+
+        1366: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            grabCursor: true,
+            speed: 1000,
+            autoplay: false,
+
+            effect: "creative",
+            creativeEffect: {
+                prev: {
+                    shadow: false,
+                    opacity: 0,
+                    translate: ["-5%", 0, -1],
+                },
+                next: {
+                    opacity: 0,
+                    translate: ["5%", 0, 0],
+                },
+            },
+
+
+            navigation: {
+                nextEl: '.gallery-next',
+                prevEl: '.gallery-prev',
+            },
+        }
+    }
+});
+
+const swiper3 = new Swiper('.swiper-gallery-container-2', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    grabCursor: true,
+    speed: 1000,
+    autoplay: false,
+
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            shadow: false,
+            opacity: 0,
+            translate: ["-5%", 0, -1],
+        },
+        next: {
+            opacity: 0,
+            translate: ["5%", 0, 0],
+        },
+    },
+
+    navigation: {
+        nextEl: '.gallery-next',
+        prevEl: '.gallery-prev',
+    },
+
+    breakpoints: {
+        375: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            grabCursor: true,
+            speed: 1000,
+            autoplay: false,
+
+            effect: "creative",
+            creativeEffect: {
+                prev: {
+                    shadow: false,
+                    opacity: 0,
+                    translate: ["-5%", 0, -1],
+                },
+                next: {
+                    opacity: 0,
+                    translate: ["5%", 0, 0],
+                },
+            },
+
+            pagination: {
+                el: '.swiper-pagination',
+                dynamicBullets: true,
+            },
+        },
+
+        1366: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            grabCursor: true,
+            speed: 1000,
+            autoplay: false,
+
+            effect: "creative",
+            creativeEffect: {
+                prev: {
+                    shadow: false,
+                    opacity: 0,
+                    translate: ["-5%", 0, -1],
+                },
+                next: {
+                    opacity: 0,
+                    translate: ["5%", 0, 0],
+                },
+            },
+
+
+            navigation: {
+                nextEl: '.gallery-next',
+                prevEl: '.gallery-prev',
+            },
+        }
+    }
+});
